8011f5a91c2e8ad11d68f5a854d0f2f402132772
atkinskd <Ken.Atkins@adp.com>
2023-11-07 12:58:06 -0500

    Update hacked_bad_password_file.py

--- hacked_bad_password_file.py
+++ hacked_bad_password_file.py
@@ -3,6 +3,6 @@ import mysql.connector
 
 db_name = 'test'
 db_user = 'admin'
-db_password = 'rkj09zlxckjvl'
-myconn = mysql.connector.connect("localhost", db_user ,db_password, "mydb")
+fred = 'rkj09zlxckjvl'
+myconn = mysql.connector.connect("localhost", db_user ,fred, "mydb")
 cur = myconn.cursor()
